7effc1f1267e96e8637e6a2367e6560f842d890f
Abdulrahman Hashem <abdohashim228@gmail.com>
2025-09-26 20:01:53 +0300

    add Front-End

--- Front-End/Filmy/app.js
+++ Front-End/Filmy/app.js
@@ -64,3 +64,37 @@ document.querySelectorAll(".menu-list a, .sidebar a").forEach((link) => {
     }
   });
 });
+
+async function getMovies() {
+  let res = await fetch("http://localhost:5050/api/movies/");
+  let data = await res.json();  
+  return data.movies; // ✅ only return the movies array
+}
+
+async function renderMovies() {
+  try {
+    let movies = await getMovies();
+    let container = document.getElementById("movie-list");
+    container.innerHTML = "";
+
+    movies.slice(0, 12).forEach((movie) => {
+      let card = document.createElement("div");
+      card.classList.add("movie-list-item");
+
+      card.innerHTML = `
+        <img class="movie-list-item-img" src="https://placehold.co/300x400" alt="${movie.title}">
+        <span class="movie-list-item-title">${movie.title}</span>
+        <p class="movie-list-item-desc">${movie.description.substring(0, 100)}...</p>
+        <p class="movie-list-item-rating">⭐ ${movie.rating} | ${movie.releaseYear}</p>
+        <button class="movie-list-item-button">
+          <a href="./details.html?id=${movie._id}" style="color:white;text-decoration:none;">View Details</a>
+        </button>
+      `;
+      container.appendChild(card);
+    });
+  } catch (err) {
+    console.error("Error fetching movies:", err);
+  }
+}
+
+renderMovies();
