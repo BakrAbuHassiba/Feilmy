63d16a7bb2f464d457ccd77da4c57ea81d3b30a0
BakrAbuHassiba <bakrfanttt@gmail.com>
2025-09-27 20:45:55 +0300

    deploy to vercel

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,24 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
-const app = express();
+import mongoose from "mongoose";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import { swaggerUi, swaggerSpec } from "./config/swagger.js";
 
-import userRoutes from "./src/routes/users.route.js";
-import movieRoutes from "./src/routes/movies.route.js";
-import reviewRoutes from "./src/routes/reviews.route.js";
-import authRoutes from "./src/routes/auth.route.js";
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection failed:", err.message));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+  credentials: true,
+}));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,19 +27,14 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../Front-End/Filmy")));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-app.use("/api/users", userRoutes);
-app.use("/api/movies", movieRoutes);
-app.use("/api/reviews", reviewRoutes);
-app.use("/api/auth", authRoutes);
+app.use("/api/users", (await import("./src/routes/users.route.js")).default);
+app.use("/api/movies", (await import("./src/routes/movies.route.js")).default);
+app.use("/api/reviews", (await import("./src/routes/reviews.route.js")).default);
+app.use("/api/auth", (await import("./src/routes/auth.route.js")).default);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../Front-End/Filmy/index.html"));
 });
-
-// app.get("/Filmy/index.html", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../Front-End/Filmy/index.html"));
-// });
 
 app.use((req, res) => {
   res.status(404).send("Not Found");
