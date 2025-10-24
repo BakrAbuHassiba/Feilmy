deb75780bf8d5cb166bf49db10733d29b86ac568
abdohshem125 <120470147+abdohshem125@users.noreply.github.com>
2025-10-24 05:01:31 +0300

    add the route of angular

--- Back-End/app.js
+++ Back-End/app.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
+      "http://localhost:4200",
       "https://subtle-bavarois-d22d70.netlify.app",
       "http://localhost:3000",
       "http://127.0.0.1:5500",
